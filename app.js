96c3067778299e3b2c9869201c0e8f81c51eeb98
Raj Vardhan Singh <srajvardhan11@gmail.com>
2024-09-27 01:35:46 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let input = document.querySelector("input");
+
+btn.addEventListener("click", function () {
+  let item = document.createElement("li");
+  item.innerText = input.value;
+
+  let del = document.createElement("button");
+  del.innerText = "delete";
+  del.classList.add("delete");
+
+  item.appendChild(del);
+  ul.appendChild(item);
+  input.value = "";
+});
+
+ul.addEventListener("click", function (event) {
+  if (event.target.nodeName == "BUTTON") {
+    let listItem = event.target.parentElement;
+    listItem.remove();
+  }
+});
